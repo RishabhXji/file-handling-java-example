9eec11f74106523e4819ec5c6d892dc851cb40a0
rishabh <rishabhjld42@gmail.com>
2026-05-06 10:53:47 +0530

    for loop , for each loop , sort method java

--- arraylist.java
+++ arraylist.java
@@ -33,6 +33,48 @@ public class arraylist {
         // check if an element exists in the list
         System.out.println(list.contains("banana")); // true
         System.out.println(list.contains("grape")); // false    
-        
+        System.out.println("*****clear method*****");
+
+        // clear the list
+        list.clear();
+        System.out.println(list);
+        System.out.println("*****isEmpty method*****");
+
+        // check if the list is empty
+        System.out.println(list.isEmpty()); // true 
+        System.out.println("*****set method*****");
+
+        // set a new value at a specific index
+        list.set(0, "mango");   
+        System.out.println(list);
+        System.out.println("*****indexOf method*****");
+        // get the index of an element
+        System.out.println(list.indexOf("mango")); // 0
+        System.out.println("*****lastIndexOf method*****");
+        // get the last index of an element
+        list.add("mango");
+        System.out.println(list.lastIndexOf("mango")); // 1
+        System.out.println("*****toArray method*****");
+        // convert the list to an array
+        Object[] array = list.toArray();
+
+        System.out.println(Arrays.toString(array));
+
+        // last index of()
+        System.out.println(list.lastIndexOf("mango")); // 1 
+        System.out.println("*****sort method*****");
+        // sort the list
+        Collections.sort(list);
+        System.out.println(list);
+        //  for lopp
+        System.out.println("*****for loop*****");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+         // for-each loop
+        System.out.println("*****for-each loop*****");
+        for (String fruit : list) {
+            System.out.println(fruit);
+        }
     }
 }
